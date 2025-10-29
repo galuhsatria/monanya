1f42c0b56afb692594722b94b312de558ce553f4
galuhsatria <galuhsatria219@gmail.com>
2025-10-29 22:59:20 +0800

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { LoaderCircle } from "lucide-react";
+import React from "react";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section>
+    <Suspense
+      fallback={
+        <div className="w-full h-screen flex justify-center items-center">
+          <div>
+            <LoaderCircle className="w-10 h-10 animate-spin" />
+          </div>
+        </div>
+      }
+    >
       {children}
-    </section>
+    </Suspense>
   );
 }
